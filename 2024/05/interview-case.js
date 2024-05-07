e301c7c2b19387d71eed9064460997ccb4180323
Cutsin <cutsin@gmail.com>
2024-05-07 20:10:15 +0800

    Create interview-case.js

---
+++ 2024/05/interview-case.js
@@ -0,0 +1,27 @@
+// Interview Case
+const sort = (str) => {
+  const arr = str.split('')
+  const sort = (arr, reverse = false) => {
+    arr.sort()
+    if (reverse) arr.reverse()
+    return arr.reduce((res, cur, i) => {
+      if (!res.includes(cur)) {
+        res.push(cur)
+        delete arr[i]
+      }
+      return res
+    }, [])
+  }
+  const result = [sort(arr), sort(arr, true), sort(arr)]
+  console.log(result)
+  return result.flat().join('')
+}
+
+// -> abccbaabc
+console.log(sort('aaabbbccc'))
+// -> acefinn
+console.log(sort('finance'))
+// -> xyzzyx
+console.log(sort('xxyyzz'))
+// -> ijkjiij
+console.log(sort('ijijkij'))
