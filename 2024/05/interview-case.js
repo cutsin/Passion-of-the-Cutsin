43d4025a6ef43254beee408065db9473985a17d9
Cutsin <cutsin@gmail.com>
2024-05-07 20:28:16 +0800

    Update interview-case.js

--- 2024/05/interview-case.js
+++ 2024/05/interview-case.js
@@ -1,18 +1,17 @@
-// Interview Case
 const sort = (str) => {
   const arr = str.split('')
-  const sort = (arr, reverse = false) => {
+  const sort = (arr, reverse = false, ignoreLast = false) => {
     arr.sort()
     if (reverse) arr.reverse()
     return arr.reduce((res, cur, i) => {
-      if (!res.includes(cur) || !arr[i+1]) {
+      if (!res.includes(cur) || (ignoreLast && !arr[i+1])) {
         res.push(cur)
         delete arr[i]
       }
       return res
     }, [])
   }
-  const result = [sort(arr), sort(arr, true), sort(arr)]
+  const result = [sort(arr), sort(arr, true), sort(arr, false, true)]
   console.log(result)
   return result.flat().join('')
 }
@@ -25,3 +24,5 @@ console.log(sort('finance'))
 console.log(sort('xxyyzz'))
 // -> ijkjiij
 console.log(sort('ijijkij'))
+console.log(sort('aaaabbc'))
+console.log(sort('aaaaaabbbdddeee'))
